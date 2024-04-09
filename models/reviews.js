6b91e0c7859a693c6f64cce0000620367cbf5559
JwThomas <nissan95sx.95@gmail.com>
2024-04-08 19:24:51 -0500

    Remove embedded repository products-list-fe

--- models/reviews.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
-  userName: String,
-  text: String,
-  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }
-});
-
-module.exports = mongoose.model('Review', reviewSchema);
