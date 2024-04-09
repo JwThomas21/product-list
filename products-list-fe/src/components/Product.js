fa73f3b149345f575088b2b60e56ccf6669e95af
JwThomas <nissan95sx.95@gmail.com>
2024-04-08 20:01:07 -0500

    Add updated files

---
+++ products-list-fe/src/components/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Product({ product }) {
+  return (
+    <div className="product">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>Category: {product.category}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
+}
+
+export default Product;
